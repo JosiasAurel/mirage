b62284fc097d29d7b2b5e736f89b46acfe560433
JosiasAurel <ndjosiasaurel@gmail.com>
2022-10-27 13:43:48 +0100

    make CLI work with clap.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,37 @@
+use clap::{command, Parser};
 use rand::Rng;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    outfile: Option<String>,
+
+    #[arg(short, long)]
+    blur: Option<f32>,
+
+    #[arg(short, long)]
+    shine: Option<i32>,
+
+    #[arg(short, long)]
+    crop: Option<String>,
+
+    #[arg(short, long)]
+    rotate: Option<String>,
+
+    #[arg(short, long)]
+    invert: Option<bool>,
+
+    // this command will make the image black and white
+    #[arg(short, long)]
+    monochrome: Option<bool>,
+
+    #[arg(short, long)]
+    fractal: Option<bool>,
+
+    #[arg(short, long)]
+    generate: Option<bool>,
+}
 
 struct CropParams {
     x: u32,
@@ -8,208 +41,216 @@ struct CropParams {
 }
 
 fn main() {
+    let commands = Args::parse();
+
+    // forgive the repetition
     let mut args: Vec<String> = std::env::args().skip(1).collect();
 
-    if args.is_empty() {
-        print_usage_and_exit();
+    let infile = args.remove(0);
+    let mut outfile = String::from("outfile.png");
+
+    match commands.outfile {
+        Some(out) => outfile = out.clone(),
+
+        None => {}
     }
-    let subcommand = args.remove(0);
 
-    match subcommand.as_str() {
-        "blur" => {
-            if args.len() < 2 || args.len() > 3 {
-                print_usage_and_exit();
-            }
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-            let blur_amount: f32;
-            if args.len() > 0 {
-                blur_amount = args.remove(0).parse().expect("Failed to parse Blur Amount");
-            } else {
-                blur_amount = 2.0;
-            }
-            blur(infile, outfile, blur_amount);
+    match commands.blur {
+        Some(amount) => {
+            blur(infile.clone(), outfile.to_string(), amount);
         }
+        None => {}
+    }
 
-        "brighten" => {
-            if args.len() < 2 || args.len() > 3 {
-                print_usage_and_exit();
-            }
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-            let inc_amt: i32;
-            if args.len() > 0 {
-                inc_amt = args
-                    .remove(0)
-                    .parse()
-                    .expect("Failed to parse brightness amount");
-            } else {
-                inc_amt = 2;
-            }
-            brighten(infile, outfile, inc_amt);
+    match commands.shine {
+        Some(amount) => {
+            brighten(infile.clone(), outfile.to_string(), amount);
         }
+        None => {}
+    }
 
-        "crop" => {
-            if args.len() != 6 {
-                print_usage_and_exit();
-            }
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-
+    match commands.crop {
+        Some(width_height) => {
+            let wh: Vec<&str> = width_height.split("-").collect();
+            let width: u32 = wh[0].parse().unwrap_or_else(|_| 0);
+            let height: u32 = wh[1].parse().unwrap_or_else(|_| 0);
             let crop_params = CropParams {
-                x: args.remove(0).parse().expect("Failed to parse x"),
-                y: args.remove(0).parse().expect("Failed to parse x"),
-                width: args.remove(0).parse().expect("Failed to parse x"),
-                height: args.remove(0).parse().expect("Failed to parse x"),
+                x: 0,
+                y: 0,
+                width,
+                height,
             };
-
-            crop(infile, outfile, &crop_params);
+            crop(infile.clone(), outfile.clone(), &crop_params);
         }
-        // Rotate -- see the rotate() function below
-        "rotate" => {
-            if args.len() < 2 || args.len() > 3 {
-                print_usage_and_exit();
-            }
+        None => {}
+    }
 
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-            let direction: String;
-            if args.len() > 0 {
-                direction = args.remove(0);
-            } else {
-                direction = String::from("right");
-            }
-
-            rotate(infile, outfile, &direction);
+    match commands.rotate {
+        Some(dir) => {
+            rotate(infile.clone(), outfile.clone(), &dir);
         }
+        None => {}
+    }
 
-        "invert" => {
-            if args.len() != 2 {
-                print_usage_and_exit();
-            }
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-
-            invert(infile, outfile);
+    match commands.invert {
+        Some(_) => {
+            invert(infile.clone(), outfile.clone());
         }
+        None => {}
+    }
 
-        "grayscale" => {
-            if args.len() != 2 {
-                print_usage_and_exit();
-            }
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-
-            grayscale(infile, outfile);
+    match commands.monochrome {
+        Some(_) => {
+            grayscale(infile.clone(), outfile.clone());
         }
-        // A VERY DIFFERENT EXAMPLE...a really fun one. :-)
-        "fractal" => {
-            if args.len() != 1 {
-                print_usage_and_exit();
-            }
-            let outfile = args.remove(0);
-            fractal(outfile);
+        None => {}
+    }
+
+    match commands.fractal {
+        Some(_) => {
+            fractal(outfile.clone());
         }
+        None => {}
+    }
 
-        "generate" => {
-            let color: [u8; 3];
-            let outfile = args.remove(0);
-            if args.len() == 0 {
-                let mut rng = rand::thread_rng();
-                color = [rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>()];
-
-                generate(outfile, &color);
-            } else if args.len() == 3 {
-                let red: u8 = args.remove(0).parse().expect("Failed to parse red value");
-                let green: u8 = args.remove(0).parse().expect("Failed to parse green value");
-                let blue: u8 = args.remove(0).parse().expect("Failed to parse blue value");
-                color = [red, green, blue];
-
-                generate(outfile, &color);
-            }
+    match commands.generate {
+        Some(_) => {
+            let mut rng = rand::thread_rng();
+            let color: [u8; 3] = [rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>()];
+            generate(outfile.clone(), &color);
         }
+        None => {}
+    }
+    /*
+             let mut args: Vec<String> = std::env::args().skip(1).collect();
 
-        _ => {
+        if args.is_empty() {
             print_usage_and_exit();
         }
-    }
-}
 
-fn print_usage_and_exit() {
-    println!("USAGE (when in doubt, use a .png extension on your filenames)");
-    let doc_string = "
-        
-> infile.png and outfile.png represent the input image file and the name of the output file, respectively
-> The extension (.png) could be any other extension like .jpeg, .jpg etc
+        if (args.len() >= 3) {
+            let mut args_ = args.clone();
+            for arg in args.iter_mut() {
+                let subcommand = args_.remove(0);
+                let infile = args_.remove(0);
+                let outfile = args_.remove(0);
+                match subcommand.as_str() {
+                    "blur" => {
+                        let blur_amount: f32;
 
-### Blur Image
+                        blur_amount = args_
+                            .remove(0)
+                            .parse()
+                            .expect("Failed to parse Blur Amount");
 
-You can blur images by running
-```shell
-./mirage input.png output.png 
-```
-The default blur amount is 2.0 but you can change that to any amout you want by passing a number as third argument.
+                        blur(infile, outfile, blur_amount);
+                    }
 
-```shell
-./mirage input.png output.png 3.4
-```
+                    "brighten" => {
+                        if args_.len() < 2 || args_.len() > 3 {
+                            print_usage_and_exit();
+                        }
+                        let infile = args_.remove(0);
+                        let outfile = args_.remove(0);
+                        let inc_amt: i32;
+                        if args_.len() > 0 {
+                            inc_amt = args_
+                                .remove(0)
+                                .parse()
+                                .expect("Failed to parse brightness amount");
+                        } else {
+                            inc_amt = 2;
+                        }
+                        brighten(infile, outfile, inc_amt);
+                    }
 
-### Grayscale Image
+                    "crop" => {
+                        if args_.len() != 6 {
+                            print_usage_and_exit();
+                        }
+                        let infile = args_.remove(0);
+                        let outfile = args_.remove(0);
 
-Make an image grayscale by running
-```shell
-./mirage grayscale input.png output.png
-```
+                        let crop_params = CropParams {
+                            x: args_.remove(0).parse().expect("Failed to parse x"),
+                            y: args_.remove(0).parse().expect("Failed to parse x"),
+                            width: args_.remove(0).parse().expect("Failed to parse x"),
+                            height: args_.remove(0).parse().expect("Failed to parse x"),
+                        };
 
-### Invert Image
-Invert an image using
-```
-./mirage invert input.png output.png 
-```
+                        crop(infile, outfile, &crop_params);
+                    }
+                    // Rotate -- see the rotate() function below
+                    "rotate" => {
+                        if args_.len() < 2 || args_.len() > 3 {
+                            print_usage_and_exit();
+                        }
 
-### Rotate Image 
-You can rotate an image `left` or `right` or make it `reverse`.
-```
-./mirage rotate input.png output.png left|right|reverse
-```
+                        let infile = args_.remove(0);
+                        let outfile = args_.remove(0);
+                        let direction: String;
+                        if args_.len() > 0 {
+                            direction = args_.remove(0);
+                        } else {
+                            direction = String::from("right");
+                        }
 
-### Brighten Image
+                        rotate(infile, outfile, &direction);
+                    }
 
-This will brighten the image by an amout of 2
-```shell
-./mirage brighten input.png output.png 
-```
-Set your own brightness by passing a number as third argument. It should be an integer. 
-Negative values will make the image dimmer while positive values will make it brighter.
-```shell
-./mirage brighten input.png output.png 4
-```
+                    "invert" => {
+                        if args_.len() != 2 {
+                            print_usage_and_exit();
+                        }
+                        let infile = args_.remove(0);
+                        let outfile = args_.remove(0);
 
-### Crop Image
+                        invert(infile, outfile);
+                    }
 
-Run
-```shell
-./mirage crop input.png output.png x y width height
-```
-Where x and y represent the initial points where to crop while width is how much to move on the x-axis and height is how much it moves from the y-axis.
-These arguments are required.
+                    "grayscale" => {
+                        if args_.len() != 2 {
+                            print_usage_and_exit();
+                        }
+                        let infile = args_.remove(0);
+                        let outfile = args_.remove(0);
 
-### Fractal Image
+                        grayscale(infile, outfile);
+                    }
 
-The CLI will generate a Julia set fractal if you run
-```shell
-./mirage fractal outfile.png
-```
+                    _ => {
+                        print_usage_and_exit();
+                    }
+                }
+                // args = args_;
+            }
+        } else {
+            let subcommand = args.remove(0);
+            match subcommand.as_str() {
+                "fractal" => {
+                    if args.len() != 1 {
+                        print_usage_and_exit();
+                    }
+                    let outfile = args.remove(0);
+                    fractal(outfile);
+                }
 
-### Generated Image
+                "generate" => {
+                    let color: [u8; 3];
+                    let outfile = args.remove(0);
+                    if args.len() == 0 {
+                        let mut rng = rand::thread_rng();
+                        color = [rng.gen::<u8>(), rng.gen::<u8>(), rng.gen::<u8>()];
 
-The CLI can also generate random beautiful patterns if you're lucky.
-```shell
-./mirage generate outfile.png
-```
-        ";
-    println!("{}", doc_string);
-    std::process::exit(-1);
+                        generate(outfile, &color);
+                    }
+                }
+                _ => {}
+            }
+        }
+    */
+
+    // let subcommand = args.remove(0);
 }
 
 fn blur(infile: String, outfile: String, blur_amount: f32) {
@@ -299,29 +340,36 @@ fn generate(outfile: String, color: &[u8; 3]) {
     ];
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let x_: i32 = if x <= 400 {
+            (x as i32) - 400
+        } else {
+            400 - (x as i32)
+        };
+        let y_: i32 = if x <= 400 {
+            (y as i32) - 400
+        } else {
+            400 - (y as i32)
+        };
+        let angle: f32 = (y_ as f32 / x_ as f32).atan();
+        let r = (x_ as f32) / angle.cos();
 
-        let x_: i32 = if x <= 400 { (x as i32)-400 } else { 400-(x as i32) };
-        let y_: i32 = if x <= 400 { (y as i32)-400 } else { 400-(y as i32) };
-        let angle: f32 = (y_ as f32/x_ as f32).atan();
-        let r = (x_ as f32)/angle.cos();
-        
         // println!("a = {}, r = {}, fn = {}, x= {}, y = {}", a1, r, trig_fn, x, y);
         let mut light_pixel = false;
         match trig_fn {
             1 => {
-                let r_c = 400.0*(a1*angle).cos();
+                let r_c = 400.0 * (a1 * angle).cos();
                 if (r_c - r).abs() < 5.0 {
                     light_pixel = true;
                 }
             }
             2 => {
-                let r_c = 400.0*(a1*angle).sin();
+                let r_c = 400.0 * (a1 * angle).sin();
                 if (r_c - r).abs() < 5.0 {
                     light_pixel = true;
                 }
             }
             3 => {
-                let r_c = 400.0*(a1*angle).tan();
+                let r_c = 400.0 * (a1 * angle).tan();
                 if (r_c - r).abs() < 5.0 {
                     light_pixel = true;
                 }
@@ -329,7 +377,7 @@ fn generate(outfile: String, color: &[u8; 3]) {
             _ => {}
         }
 
-        if  light_pixel {
+        if light_pixel {
             *pixel = image::Rgb(point_color);
         } else {
             *pixel = image::Rgb(*color);
@@ -374,4 +422,3 @@ fn fractal(outfile: String) {
 
     imgbuf.save(outfile).unwrap();
 }
-
